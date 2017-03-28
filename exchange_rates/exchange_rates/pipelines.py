3df227d34069d27733075e71c8b0d88cf4cb2de4
U-Kevin-PC\Kevin <kevpcmaint@hotmail.co.uk>
2017-03-28 11:02:16 +0100

    Added duplicates filter pipeline

--- exchange_rates/exchange_rates/pipelines.py
+++ exchange_rates/exchange_rates/pipelines.py
@@ -1,11 +1,13 @@
-# -*- coding: utf-8 -*-
+from scrapy.exceptions import DropItem
 
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+class DuplicatesPipeline(object):
 
+    def __init__(self):
+        self.ids_seen = set()
 
-class ExchangeRatesPipeline(object):
     def process_item(self, item, spider):
-        return item
+        if item['Currency'] in self.ids_seen:
+            raise DropItem("Duplicate item found: %s" % item)
+        else:
+            self.ids_seen.add(item['Currency'])
+            return item
